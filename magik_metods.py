c5138e8f6b58c72415ffc4e0636a49fdbc9b8424
ArtemMokhonko <artem.stayl@gmail.com>
2024-05-28 15:08:36 +0300

    1

--- magik_metods.py
+++ magik_metods.py
@@ -65,16 +65,57 @@
 # # Використання індексів для вказівки порядку аргументів
 # print('Hello, {1}. You are {0} years old.'.format(age, name))
 
+import pickle
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
 
-def discount_price(price:int, discount: float):
-    def apply_discount():
-        nonlocal price 
-        apply_discount(price * (1 - discount))
-        
-        
 
+class Contacts:
     
-    return price
+    def __init__(self, filename: str,  contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+        self.count_save = 0
+    
+    def save_to_file(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self, file)
 
-res = discount_price(100, 0.5)
-print(res)
+    def read_from_file(self):
+        with open(self.filename, "rb") as file:
+            content = pickle.load(file)
+        return content
+
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        attributes["count_save"] = attributes["count_save"] + 1
+        return attributes
+               
+        
+    
+    
+bob = Person ("bob", "2dfdf", 5544654,True)
+persons = Contacts("user_class.dat", bob)
+persons.save_to_file()
+first = persons.read_from_file()
+first.save_to_file()
+second = first.read_from_file()
+second.save_to_file()
+third = second.read_from_file()
+second.save_to_file()
+third = second.read_from_file()
+second.save_to_file()
+third = second.read_from_file()
+second.save_to_file()
+third = second.read_from_file()
+
+print(persons.count_save)  # 0
+print(first.count_save)  # 1
+print(second.count_save)  # 2
+print(third.count_save)  # 3
